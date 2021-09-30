50853e07f013d03285022c70df77df3393ab8a55
NguyenThanhAn201 <fxannguyen201@gmail.com>
2021-09-30 19:41:12 +0700

    Add Loading

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Loading.scss";
+
+const Loading = () => {
+  return (
+    <div class="lds-ellipsis">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loading;
