e5b2fb1dc2165f48342af0d7bfa348a6fe112dff
NguyenThanhAn201 <fxannguyen201@gmail.com>
2021-09-30 21:17:33 +0700

    Edit Popup

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -56,6 +56,23 @@ const HomePage = () => {
     );
   }
 
+  function idMeasure(measure) {
+    let arr = [
+      measure.strMeasure1,
+      measure.strMeasure2,
+      measure.strMeasure3,
+      measure.strMeasure4,
+      measure.strMeasure5,
+    ];
+    return (
+      <>
+        {arr.map((val, index) => (
+          <p key={index}>{val}</p>
+        ))}
+      </>
+    );
+  }
+
   return (
     <>
       <div className="home">
@@ -74,82 +91,90 @@ const HomePage = () => {
           <button onClick={fetchMealsHandler}>Search Meal</button>
         </div>
         <div className="home-grid">
-          {loading ? (
-            meals ? (
-              meals.map((meal, index) => (
-                <div key={index}>
-                  <div className="home-meal">
-                    <img src={meal.strMealThumb} alt="meal" />
-                    <h4>{meal.strMeal}</h4>
-                    <button
-                      className="btn-ingredient"
-                      onClick={() => {
-                        setShow(true);
-                        setIdMeal(index);
-                      }}
-                    >
-                      Ingredients
-                    </button>
-                    <br />
-                    <button
-                      className="btn-seemore"
-                      onClick={() => window.open(meal.strYoutube)}
-                    >
-                      See Complete Recipe
-                    </button>
-                  </div>
+          {meals ? (
+            meals.map((meal, index) => (
+              <div key={index}>
+                <div className="home-meal">
+                  <img src={meal.strMealThumb} alt="meal" />
+                  <h4>{meal.strMeal}</h4>
+                  <button
+                    className="btn-ingredient"
+                    onClick={() => {
+                      setShow(true);
+                      setIdMeal(index);
+                    }}
+                  >
+                    Ingredients
+                  </button>
+                  <br />
+                  <button
+                    className="btn-seemore"
+                    onClick={() => window.open(meal.strYoutube)}
+                  >
+                    See Complete Recipe
+                  </button>
+                </div>
 
-                  <Dialog open={show}>
-                    <DialogContent>
-                      <DialogActions>
-                        <button
-                          className="btn recipe-close-btn"
-                          onClick={() => setShow(false)}
-                        >
-                          <i className="fas fa-times" />
-                        </button>
-                      </DialogActions>
-                      {/* Nhung Noi Dung Phan Popup */}
-                      <h2 className="recipe-title">{meals[idMeal].strMeal}</h2>
-                      <p className="recipe-category">
-                        {meals[idMeal].strCategory}
-                      </p>
-                      <div class="recipe-instruct">
-                        <h3>Instructions:</h3>
-                        <p>{meals[idMeal].strInstructions}</p>
-                      </div>
-                      <img src={meals[idMeal].strMealThumb} alt="meal" />
-                      {idIngredient(meals[idMeal])}
-                      <a
-                        className="recipe-link"
-                        onClick={() => window.open(meals[idMeal].strYoutube)}
+                <Dialog open={show}>
+                  <DialogContent>
+                    <DialogActions>
+                      <button
+                        className="btn recipe-close-btn"
+                        onClick={() => setShow(false)}
                       >
-                        See On <i class="fab fa-youtube"></i>
-                      </a>
-                    </DialogContent>
-                  </Dialog>
-                </div>
-              ))
-            ) : (
-              <>
-                <div className="no-search">
-                  <div className="content">
-                    <h1>DON'T CRY!</h1>
-                    <p>
-                      The page you're looking for isn't here. Hit up our
-                      homepage for some delicious recipe ideas. We'll clean up
-                      the spill.
+                        <i className="fas fa-times" />
+                      </button>
+                    </DialogActions>
+                    {/* Nhung Noi Dung Phan Popup */}
+                    <h2 className="recipe-title">{meals[idMeal].strMeal}</h2>
+                    <p className="recipe-category">
+                      {meals[idMeal].strCategory}
                     </p>
-                  </div>
-                  <img
-                    src="https://www.epicurious.com/static/img/error/404-1024.png"
-                    alt="#"
-                  ></img>
-                </div>
-              </>
-            )
+                    <div class="recipe-instruct">
+                      <h3>Instructions:</h3>
+                      <p>{meals[idMeal].strInstructions}</p>
+                    </div>
+                    <img src={meals[idMeal].strMealThumb} alt="meal" />
+                    <table>
+                      <thead>
+                        <tr>
+                          <th>Ingredients</th>
+                          <th>Measure</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>{idIngredient(meals[idMeal])}</td>
+                          <td>{idMeasure(meals[idMeal])}</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <a
+                      className="recipe-link"
+                      onClick={() => window.open(meals[idMeal].strYoutube)}
+                    >
+                      See On <i class="fab fa-youtube"></i>
+                    </a>
+                  </DialogContent>
+                </Dialog>
+              </div>
+            ))
           ) : (
-            <Loading />
+            <>
+              <div className="no-search">
+                <div className="content">
+                  <h1>DON'T CRY!</h1>
+                  <p>
+                    The page you're looking for isn't here. Hit up our homepage
+                    for some delicious recipe ideas. We'll clean up the spill.
+                  </p>
+                </div>
+                <img
+                  src="https://www.epicurious.com/static/img/error/404-1024.png"
+                  alt="#"
+                ></img>
+              </div>
+            </>
           )}
         </div>
       </div>
